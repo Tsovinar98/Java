49fa95d6c92c2e411f806927eef508722813128d
Tsovinar Gasparyan <Tsovinar1998@gmail.com>
2018-06-09 22:20:03 -0400

    testing third line

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String args[]){
         System.out.println("Hello World!dfnfdn");
         System.out.println("but does it work?");
+        System.out.println("testing");
     }
 }
