fcc4c9e0045f6fa11c6db31494d2502277736628
Tsovinar98 <33967268+Tsovinar98@users.noreply.github.com>
2018-06-09 22:21:18 -0400

    added second line

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello World!dfnfdn");
         System.out.println("but does it work?");
         System.out.println("testing");
+        System.out.println("testing2");
     }
 }
